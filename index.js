20d453ccb91d2d15e368f25acaef87f3a783348e
Jack L <Jack L>
2016-06-25 17:52:59 -0400

    Adding traffic API

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ let Bot  = require('@kikinteractive/kik');
 let bot = new Bot({
     username: 'trafficwatch',
     apiKey: '3d46fc64-c8a7-4b7b-b78d-339b9e335600',
-    baseUrl: 'https://d160c049.ngrok.io'
+    baseUrl: 'https://80c7e18d.ngrok.io'
 });
 
 bot.updateBotConfiguration();
@@ -20,13 +20,13 @@ bot.onTextMessage((message) => {
                 message.reply(`Hey ${user.firstName}! Hope you are doing fine!`);
             });
     } else {
-        
+
     }
 });
 
 // // To one user (a.username)
 // bot.send(Bot.Message.text('Hey, nice to meet you!'), 'iocds');
-// 
+
 // // You can use a shorthand for text messages to keep things a bit cleaner
 // bot.send('Getting started is super easy!', 'iocds');
 
@@ -34,7 +34,7 @@ bot.onTextMessage((message) => {
 let server = http
     .createServer(bot.incoming())
     .listen(process.env.PORT || 8080);
-    
+
 server.on('request', function(req, res){
     console.log(req.method);
     console.log(req.headers);
--- trafficRequest.js
+++ trafficRequest.js
@@ -1,15 +1,38 @@
 var request = require('request');
+var lat = 43.6534922;
+var lon = -79.3839729;
+var z = 10,        // Zoom level
+latRad,
+n,
+xTile,
+yTile;
 
-request.post({
-    url : 'http://dev.virtualearth.net/REST/v1/Traffic/Incidents/mapArea/includeLocationCodes?severity=severity1,severity2,severityn&type=type1,type2,typen&key=sSD8GTh2fjzOCK5MQY48~TOeiR5xGWwYMRbIWKIhG7Q~AgCkIOmyG5pn-1QDaKefJ9PPfrx3i9oZLpRuJ5vwazPQOCTRbb_kNdnb43DdsTiW',
-    headers:{
-        'Content-Type': 'application/json'
+latRad = lat * Math.PI / 180;
+n = Math.pow(2, z);
+xTile = n * ((lon + 180) / 360);
+yTile = n * (1-(Math.log(Math.tan(latRad) + 1/Math.cos(latRad)) / Math.PI)) / 2;
+
+var requestUrl = 'https://traffic.cit.api.here.com/traffic/6.0/incidents/json' +
+                  '/' + z + '/' + Math.floor(xTile) + '/' + Math.floor(yTile);
+
+// console.log(requestUrl);
+
+request.get({
+    url : requestUrl,
+    qs:{
+        'app_id': 'MvDdxhQdDnQMCQxVG2u9',
+        'app_code': 'mEDyU_ALl5rNUSsDSbteDQ',
+        'c': 'CA',
+        'lg': 'en',
+        'il8n': true,
+        'localtime': true
+
     }
 }, function(err, res, body){
     if(err){
         console.log(err);
     } else {
+        console.log(res.statusCode);
         console.log(body);
     }
-    console.log(body);
 });
